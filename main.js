a3be0fe833347bde219996be76e9357f2dbc0c68
nafees96 <42964150+nafees96@users.noreply.github.com>
2018-10-23 14:20:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// //variable type string
+
+
+var count = '3 '
+var animals = 'dogs '
+var country = 'America'
+var other = 'and I live in ' 
+
+//console shows name
+console.log('My name is Sally and own ' + count + animals + other + country);
+
